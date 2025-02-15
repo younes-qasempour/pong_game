fd085f3ff2da455ee3f0ee640d0091bab4a15f9b
younes_qasempour <qasempouryounes@gmail.com>
2025-02-15 22:42:30 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+screen.listen()
+
+
+
+
+
+
+
+screen.exitonclick()
