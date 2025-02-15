2c4bfca8bfbf1ca4afa80189eda35810ca2609cb
younes_qasempour <qasempouryounes@gmail.com>
2025-02-16 00:22:25 +0330

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from turtle import Screen
-
+from ball import Ball
+from paddle import Paddle
+import time
+#TODO-1 Start from video 7
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
+
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
 screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # Detect collision by wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
 
 
 
